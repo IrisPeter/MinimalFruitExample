d414b4cf8b99108588584d86c2dc863aa76d9c1e
Peter Nimmo <Peter.Nimmo@iris.co.uk>
2022-09-26 16:39:03 +0100

    Subsitute return type so that is uses std::unique_ptr This now builds but we don't yet now how to make ourselves a AssetGroup

--- AssetGroup.cpp
+++ AssetGroup.cpp
@@ -11,9 +11,15 @@ private:
 
 public:
 	INJECT(AssetGroupImpl(ASSISTED(int) group)) : m_group(group) {}
+
+	// Inherited via IAssetGroup
+	bool IsMotorGroup() const override
+	{
+		return false;
+	}
 };
 
-fruit::Component<std::function<IAssetGroup(int)>> getAssetGroupComponent()
+fruit::Component<std::function<std::unique_ptr<IAssetGroup>(int)>> getAssetGroupComponent()
 {
 	return fruit::createComponent()
 		.bind<IAssetGroup, AssetGroupImpl>();
--- AssetGroup.h
+++ AssetGroup.h
@@ -1,13 +1,15 @@
 #pragma once
 
 #include <functional>
+#include <memory>
 
 #include <fruit/fruit.h>
 
 class IAssetGroup
 {
 public:
+    virtual ~IAssetGroup() = default;
     virtual bool IsMotorGroup() const = 0;
 };
 
-fruit::Component<std::function<IAssetGroup(int)>> getAssetGroupComponent();
+fruit::Component<std::function<std::unique_ptr<IAssetGroup>(int)>> getAssetGroupComponent();
